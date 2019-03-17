6a5182861005f5e49f92e58580d7c26184dd7c16
zhao dong <zhaolinpu@gmail.com>
2019-03-17 21:43:06 +0800

    basic auth

--- readme.txt
+++ readme.txt
@@ -1,18 +1,13 @@
 
-(1). Training Video
-
-https://www.youtube.com/watch?v=Yoa5baPv-1A
 
 
-(2). auto setup
-hibernate.hbm2ddl.auto节点的值有几个create、create-drop、update、validate、none
+command:
+curl -i --user user1:user1Pass http://localhost:8082/
 
-create：每次加载hibernate会自动创建表，以后启动会覆盖之前的表，所以这个值基本不用，严重会导致的数据的丢失。
 
-create-drop ： 每次加载hibernate时根据model类生成表，但是sessionFactory一关闭，表就自动删除，下一次启动会重新创建。
+(1). git hub reference
+https://github.com/eugenp/tutorials/blob/master/spring-security-rest-basic-auth/src/main/java/org/baeldung/filter/CustomWebSecurityConfigurerAdapter.java
 
-update：加载hibernate时根据实体类model创建数据库表，这是表名的依据是@Entity注解的值或者@Table注解的值，sessionFactory关闭表不会删除，且下一次启动会根据实体model更新结构或者有新的实体类会创建新的表。
+(2). reference doc for basic auth
 
-validate：启动时验证表的结构，不会创建表
-
-none：启动时不做任何操作
+https://www.baeldung.com/spring-security-basic-authentication
--- src/main/java/com/zd/secureapp/AppSecurityConfig.java
+++ src/main/java/com/zd/secureapp/AppSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,7 +16,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -73,6 +76,11 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	*/
 	
+	
+	
+	
+	/* 
+	
 	@Autowired
 	private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
 	
@@ -100,4 +108,41 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
     }
+	
+	*/
+	
+	
+	
+	@Autowired
+	private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
+	
+
+	    @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	        auth
+	          .inMemoryAuthentication()
+	          .withUser("user1")
+	          .password(passwordEncoder().encode("user1Pass"))
+	          .authorities("ROLE_USER");
+	    }
+
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	          .authorizeRequests()
+	          .antMatchers("/securityNone")
+	          .permitAll()
+	          .anyRequest()
+	          .authenticated()
+	          .and()
+	          .httpBasic()
+	          .authenticationEntryPoint(authenticationEntryPoint);
+
+	        http.addFilterAfter(new CustomFilter(), BasicAuthenticationFilter.class);
+	    }
+	    
+	    @Bean
+	    public PasswordEncoder passwordEncoder() {
+	        return new BCryptPasswordEncoder();
+	    }
 }
