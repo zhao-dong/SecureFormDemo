4216443f98a4330370100bb75163826fe3a9dfba
zhao-dong <zhaolinpu@gmail.com>
2019-03-19 14:19:53 +0800

    temp job

--- src/main/java/com/zd/secureapp/SecureAppApplication.java
+++ src/main/java/com/zd/secureapp/SecureAppApplication.java
@@ -2,6 +2,13 @@ package com.zd.secureapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserCache;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.cache.SpringCacheBasedUserCache;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
 
 @SpringBootApplication
 public class SecureAppApplication {
@@ -9,5 +16,5 @@ public class SecureAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SecureAppApplication.class, args);
 	}
-
+	
 }
--- src/main/java/com/zd/secureapp/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zd.secureapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<TestUser, Long> {
-
-	TestUser findByUsername(String username);
-}
